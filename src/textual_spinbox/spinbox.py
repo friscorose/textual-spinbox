871930570b7135ff59b389143d5d547035d518e1
Frisco Rose <friscorose@gmail.com>
2024-11-30 18:43:20 -0500

    drag up/down trigger value change

--- src/textual_spinbox/spinbox.py
+++ src/textual_spinbox/spinbox.py
@@ -1,13 +1,34 @@
-from textual import events, on
-from textual.pad import HorizontalPad
 from rich.text import Text, TextType
-from textual.app import RenderResult
 
-from textual.app import App, ComposeResult
+from textual import events, on
+from textual.app import App, ComposeResult, RenderResult
 from textual.containers import Horizontal, Vertical
+from textual.events import MouseScrollDown, MouseScrollUp
+from textual.pad import HorizontalPad
 from textual.widgets import Button, Input, Label
 
 class CellButton( Button, can_focus=False ):
+
+    def on_mouse_up(self, event):
+        event.stop()
+        if self.id == "sb_up":
+            self.post_message( MouseScrollUp(self,
+                                             event.x,
+                                             event.y,
+                                             event.delta_x,
+                                             event.delta_y,
+                                             1, 0, 0, 0) )
+        elif self.id == "sb_dn":
+            self.post_message( MouseScrollDown(self,
+                                             event.x,
+                                             event.y,
+                                             event.delta_x,
+                                             event.delta_y,
+                                             1, 0, 0, 0) )
+
+    def on_mouse_down(self, event):
+        event.stop()
+
     def render(self) -> RenderResult:
             assert isinstance(self.label, Text)
             label = self.label.copy()
@@ -23,18 +44,41 @@ class CellButton( Button, can_focus=False ):
 
 class SpinBox(Horizontal):
 
+    draggable = False
+    dragged = False
+    def on_mouse_move(self, event):
+        if self.draggable and event.delta_y < 0:
+            self.dragged = True
+            self.post_message( MouseScrollUp(self,
+                                             event.x,
+                                             event.y,
+                                             event.delta_x,
+                                             event.delta_y,
+                                             1, 0, 0, 0) )
+        elif self.draggable and event.delta_y > 0:
+            self.dragged = True
+            self.post_message( MouseScrollDown(self,
+                                               event.x,
+                                               event.y,
+                                               event.delta_x,
+                                               event.delta_y,
+                                               1, 0, 0, 0) )
+
+    def on_mouse_up(self, event):
+        self.draggable = False
+        if self.dragged:
+            self.dragged = False
+        self.release_mouse()
+
+    def on_mouse_down(self, event):
+        self.draggable = True
+        self.capture_mouse()
+
     def on_mouse_scroll_up(self):
         self.delta_v( 1 )
 
     def on_mouse_scroll_down(self):
         self.delta_v( -1 )
-
-    @on( Button.Pressed )
-    def click_handler(self, event):
-        if event.button.id == "sb_up":
-            self.delta_v( 1 )
-        if event.button.id == "sb_dn":
-            self.delta_v( -1 )
 
     def delta_v( self, dv ):
         foo = int( self.query_one("#sb_field").value ) + dv
@@ -46,7 +90,7 @@ class SpinBox(Horizontal):
             yield Input("0", type="integer", id="sb_field")
             with Vertical( id="sb_control" ):
                 yield CellButton("▲", id="sb_up" )
-                yield Label("|")
+                yield Label("¦")
                 yield CellButton("▼", id="sb_dn" )
 
 class SpinboxApp(App):
